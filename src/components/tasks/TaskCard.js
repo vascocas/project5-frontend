37ba8c84e246dc0ce95b525316e2580808ec2fe9
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-03-30 15:17:33 +0000

    move task

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -9,10 +9,7 @@ function TaskCard({ title, priority, taskId, state, creator }) {
   const { token } = userStore();
   const { role } = userStore(state => state);
     // State variables for visibility of options and move options
-  const [showOptions, setShowOptions] = useState(false);
-  const [showMoveOptions, setShowMoveOptions] = useState(false);
-  // State variable for selected column
-  const [selectedColumn, setSelectedColumn] = useState(""); 
+  const [showOptions, setShowOptions] = useState(false); 
   const { deleteTask, updateTask } = taskStore();
   const navigate = useNavigate();
 
@@ -58,39 +55,22 @@ function TaskCard({ title, priority, taskId, state, creator }) {
   };
 
   // Handle Move Task
-  const handleMove = () => {
-    setShowOptions(false);
-    setShowMoveOptions(true);
-  };
-
-  const handleDropdownChange = (e) => {
-    setSelectedColumn(e.target.value);
-  };
-
-  // Handle Move Task Cancel
-  const handleMoveCancel = () => {
-    setShowMoveOptions(false); // Hide move options
-    setShowOptions(true); // Show the three option buttons
-  };  
-
-  // Handle Move Task Confirmation
-  const handleMoveConfirm = async () => {
-    if (!selectedColumn) {
-      alert("Please select a column.");
-      return;
-    }
-
-    if (selectedColumn === state) {
-      alert("Task is already in the selected column.");
-      return;
-    }
+  const handleMove = async () => {
+     // Determine next state
+     let nextState;
+     if (state === "TODO") {
+         nextState = "DOING";
+     } else if (state === "DOING") {
+         nextState = "DONE";
+     } else {
+         return; // Task is already in DONE state
+     }
 
     try {
       const requestBody = JSON.stringify({
         id: taskId,
-        state: selectedColumn,
+        state: nextState,
       });
-
       const response = await fetch(
         "http://localhost:8080/project5-backend/rest/tasks/status",
         {
@@ -107,12 +87,11 @@ function TaskCard({ title, priority, taskId, state, creator }) {
         const successMessage = await response.text();
         console.log(successMessage);
         // Hide all options after moving
-        setShowMoveOptions(false);
         setShowOptions(false);
         // Update task state
         updateTask({
           id: taskId,
-          state: selectedColumn,
+          state: nextState,
           priority: priority,
           title: title,
           creator: creator,
@@ -140,7 +119,7 @@ function TaskCard({ title, priority, taskId, state, creator }) {
     }
     // If the creator is not erased, return the original title
     return title;
-  };
+  }; 
 
   return (
     <div
@@ -150,35 +129,18 @@ function TaskCard({ title, priority, taskId, state, creator }) {
     >
       <div className="card-header">{generateTaskTitle()}</div>
       <div className="task-options">
-        {showOptions && !showMoveOptions && (
-          <>
-            <button className="tasks-button" onClick={handleMove}>
+      {showOptions && state !== "DONE" && (
+      <button className="tasks-button" onClick={handleMove}>
               Move
-            </button>
+            </button>)}
+            {showOptions && (
             <button className="tasks-button" onClick={handleConsult}>
               Consult
-            </button>
-            {(role === "PRODUCT_OWNER" || role === "SCRUM_MASTER") && (<button className="tasks-button" onClick={handleRemove}>
+            </button>)}
+            {showOptions && (role === "PRODUCT_OWNER" || role === "SCRUM_MASTER") && (
+            <button className="tasks-button" onClick={handleRemove}>
               Remove
             </button>)}
-          </>
-        )}
-        {showMoveOptions && (
-          <>
-            <select  className="custom-select" value={selectedColumn} onChange={handleDropdownChange}>
-              <option value="">Select a column</option>
-              <option value="TODO">TO DO</option>
-              <option value="DOING">DOING</option>
-              <option value="DONE">DONE</option>
-            </select>
-            <button className="tasks-button" onClick={handleMoveConfirm}>
-              Confirm Move
-            </button>
-            <button className="tasks-button" onClick={handleMoveCancel}>
-              Cancel
-            </button>
-          </>
-        )}
       </div>
     </div>
   );
