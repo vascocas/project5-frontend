2bcf1fc5317fbe1bf1745ab3899b8a25c3b7f188
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-04-23 16:22:50 +0100

    MediaType

--- src/components/media/MediaType.js
+++ src/components/media/MediaType.js
@@ -1,35 +1,38 @@
 import { useState, useEffect } from "react";
 import { userStore } from "../../stores/UserStore";
+
 function MediaType() {
-const updateMediatype = userStore((state) => state.updateMediatype);
-const [mediaType, setMediaType] = useState({
-isDesktopOrLaptop: false,
-isBigScreen: false,
-isTabletOrMobile: false,
-isPortrate: false,
-isRetina: false
-});
-const handleResize = () => {
-setMediaType({
-isDesktopOrLaptop: window.matchMedia('(min-width: 1224px)').matches,
-isBigScreen: window.matchMedia('(min-width: 1824px)').matches,
-isTabletOrMobile: window.matchMedia('(max-width: 1224px)').matches,
-isPortrate: window.matchMedia('(orientation: portrait)').matches,
-isRetina: window.matchMedia('(min-resolution: 2pppx)').matches
-});
-};
-useEffect(() => {
-// Initial check
-handleResize();
-// Add event listener
-window.addEventListener("resize", handleResize);
-// Cleanup
-return () => {
-window.removeEventListener("resize", handleResize);
-};
-}, []);
-useEffect(() => {
-updateMediatype(mediaType);
-}, [mediaType, updateMediatype]);
+  const updateMediatype = userStore((state) => state.updateMediatype);
+  const [mediaType, setMediaType] = useState({
+    isBigScreen: false,
+    isSmallScreen: false,
+    isMobile: false,
+    isPortrait: false,
+  });
+
+  const handleResize = () => {
+    setMediaType({
+      isBigScreen: window.matchMedia("(min-width: 1224px)").matches,
+      isSmallScreen: window.matchMedia("(min-width: 1024px)").matches,
+      isMobile: window.matchMedia("(max-width: 1024px)").matches,
+      isPortrait: window.matchMedia("(orientation: portrait)").matches,
+    });
+  };
+
+  useEffect(() => {
+    // Initial check
+    handleResize();
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Cleanup
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    updateMediatype(mediaType);
+  }, [mediaType, updateMediatype]);
 }
 export default MediaType;
